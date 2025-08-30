c3c80af823ac9f2852e7c02d83ce395eeb68c959
Esa <esa-pitkanen@hotmail.com>
2025-08-30 14:40:59 +0300

    Test a linked list

--- main.js
+++ main.js
@@ -4,5 +4,8 @@ const list = new LinkedList()
 list.append("dog");
 list.append("cat");
 list.append("parrot");
-let test = list.pop();
-console.log(test);
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
